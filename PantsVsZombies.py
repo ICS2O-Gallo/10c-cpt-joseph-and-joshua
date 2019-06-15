d966bea42f4a5ad8afde224e5b94993ed9cc6579
MECOOCOO <47458329+MECOOCOO@users.noreply.github.com>
2019-06-15 02:08:13 -0400

    Make zombies objects.

--- PantsVsZombies.py
+++ PantsVsZombies.py
@@ -1,10 +1,7 @@
 from arcade import *
 
-Width = 1200
-Height = 800
-
-button_1_Width = 200
-button_1_Height = 700
+WIDTH = 1200
+HEIGHT = 800
 
 # button stuff
 BTN_X = 0
@@ -14,13 +11,18 @@ BTN_HEIGHT = 3
 BTN_IS_CLICKED = 4
 BTN_COLOR = 5
 BTN_CLICKED_COLOR = 6
+button1 = [200, 200, 300, 50, False, color.BLUE, color.RED]
+button2 = [200, 140, 100, 30, False, color.BLUE, color.RED]
 
-# different screens
-current_screen = "menu"
-instruction_button = [(Width - 190) / 2, Height - 560, 190, 50, color.GRAY, color.RED]
-game_button = [(Width - 190) / 2, Height - 710, 190, 50, color.BLACK, color.BLUE]
+# Menu: 0
+# Instructions: 1
+# Game: 2
+current_screen = 0
 
-button_1 = [button_1_Width, button_1_Height, 100, 100, False, color.BLUE, color.RED]
+instruction_button = [(WIDTH - 190) / 2, HEIGHT - 560, 190, 50, color.GRAY, color.RED]
+game_button = [(WIDTH - 190) / 2, HEIGHT - 710, 190, 50, color.BLACK, color.BLUE]
+
+button_1 = [WIDTH - 1000, HEIGHT - 100, 100, 100, False, color.BLUE, color.RED]
 
 # grid
 grid_col_1 = color.GREEN
@@ -36,9 +38,28 @@ selected = -1
 zombie_x_positions = [800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800]
 zombie_y_positions = [130, 250, 450, 230, 13, 100, 52, 340, 259, 636, 547]
 
+class Zombie:
+
+
+    def __init__(self, row, speed='2.5', color=color.BLUE, health=10):
+        self.y = [0, 100, 200, 300, 400, 500, 600, 700][row]
+        self.speed = speed
+        self.color = color
+        self.health = health
+        self.x = 800
+
+    def walk(self):
+        self.x -= self.speed
+        if self.x <= 0:
+            pass # TODO: Handle game over
+
+    def hit(self, damage=1):
+        self.health -= damage
+        if self.health <= 0:
+            pass # TODO: Handle zombie death
 
 def setup():
-    open_window(Width, Height, "My Arcade Game")
+    open_window(WIDTH, HEIGHT, "My Arcade Game")
     set_background_color(color.WHITE)
     schedule(update, 1 / 60)
     window = get_window()
@@ -69,28 +90,28 @@ def on_draw():
     start_render()
 
     # menu
-    if current_screen == "menu":
+    if current_screen == 0:
         set_background_color(color.GREEN)
-        draw_text("Pants VS Zombies", Width / 2, Height
-                  - 100, color.BLACK, font_size=30,
-                  anchor_x="center")
+        draw_text("Pants VS Zombies", WIDTH / 2, HEIGHT
+                         - 100, color.BLACK, font_size=30,
+                         anchor_x="center")
         draw_instruction_button(instruction_button)
         draw_game_button(game_button)
-        draw_text("Instructions", Width / 2, Height - 550, color.BLACK, font_size=30,
-                  anchor_x="center")
-        draw_text("Play", Width / 2, Height - 700, color.BLACK, font_size=30, anchor_x="center")
+        draw_text("Instructions", WIDTH / 2, HEIGHT - 550, color.BLACK, font_size=30,
+                         anchor_x="center")
+        draw_text("Play", WIDTH / 2, HEIGHT - 700, color.BLACK, font_size=30, anchor_x="center")
 
     # instructions
-    elif current_screen == "Instructions":
+    elif current_screen == 1:
         set_background_color(color.GRAY)
-        draw_text("Instructions", Width / 2, Height - 100, color.BLACK, font_size=30,
-                  anchor_x="center")
+        draw_text("Instructions", WIDTH / 2, HEIGHT - 100, color.BLACK, font_size=30,
+                         anchor_x="center")
         draw_instructions("instructions")
-        draw_text("Esc to go to Menu", Width - 1000, Height - 700, color.BLACK, font_size=30,
-                  anchor_x="center")
+        draw_text("Esc to go to Menu", WIDTH - 1000, HEIGHT - 700, color.BLACK, font_size=30,
+                         anchor_x="center")
         # game
 
-    elif current_screen == "Game":
+    elif current_screen == 2:
         # grid
         setup_grid(grid_col_1, grid_col_2)
         draw_piece()
@@ -98,7 +119,6 @@ def on_draw():
         # zombies
         for c, d in zip(zombie_x_positions, zombie_y_positions):
             draw_circle_filled(c, d, 30, color.BLUE)
-        draw_button_1(button_1)
 
 
 def setup_grid(color_1, color_2):
@@ -142,12 +162,12 @@ def draw_piece():
 def on_key_press(key, modifiers):
     global current_screen
 
-    if current_screen == "Instructions":
-        if key == arcade.key.ESCAPE:
-            current_screen = "menu"
-    elif current_screen == "Game":
-        if key == arcade.key.ESCAPE:
-            current_screen = "menu"
+    if current_screen == 1:
+        if key == key.ESCAPE:
+            current_screen = 0
+    elif current_screen == 2:
+        if key == key.ESCAPE:
+            current_screen = 0
 
 
 def on_key_release(key, modifiers):
@@ -163,24 +183,19 @@ def on_mouse_press(x, y, button, modifiers):
     print(f"Click at ({x}, {y})")
     global current_screen
 
+    if mouse_hover(x, y, button1):
+        button1[BTN_IS_CLICKED] = True
+    if mouse_hover(x, y, button2):
+        button2[BTN_IS_CLICKED] = True
+
     # menu screen
     if mouse_hover(x, y, instruction_button):
         instruction_button[BTN_IS_CLICKED] = True
-        current_screen = "Instructions"
+        current_screen = 1
 
     elif mouse_hover(x, y, game_button):
         game_button[BTN_IS_CLICKED] = True
-        current_screen = "Game"
-
-    elif mouse_hover(x, y, button_1):
-        button_1[BTN_IS_CLICKED] = True
-
-
-def on_mouse_drag(x, y, button, modifiers):
-    if button_1[BTN_IS_CLICKED]:
-        while True:
-            button_1_Width = mouse_motion(x, y, dx, dy)
-            button_1_Height = mouse_motion(x, y, dx, dy)
+        current_screen = 2
 
 
 def mouse_motion(x, y, dx, dy):
@@ -193,11 +208,31 @@ def mouse_motion(x, y, dx, dy):
 
 
 def on_mouse_release(x, y, button, modifiers):
-    button_1[BTN_IS_CLICKED] = False
+    button1[BTN_IS_CLICKED] = False
+    button2[BTN_IS_CLICKED] = False
+
 
 def mouse_hover(x, y, button) -> bool:
     return x > button[BTN_X] and x < button[BTN_X] + button[BTN_WIDTH] and y > button[BTN_Y] \
         and y < button[BTN_Y] + button[BTN_HEIGHT]
+
+def draw_button(button):
+    # Select the appropriate color to draw with
+    if button1[BTN_IS_CLICKED]:
+        color = button1[BTN_CLICKED_COLOR]
+    else:
+        color = button1[BTN_COLOR]
+
+    if button2[BTN_IS_CLICKED]:
+        color = button2[BTN_CLICKED_COLOR]
+    else:
+        color = button2[BTN_COLOR]
+        # Draw button1
+    draw_xywh_rectangle_filled(button[BTN_X],
+                                      button[BTN_Y],
+                                      button[BTN_WIDTH],
+                                      button[BTN_HEIGHT],
+                                      color)
 
 
 def draw_button_1(button_1):
@@ -206,10 +241,10 @@ def draw_button_1(button_1):
     else:
         color = [BTN_COLOR]
     draw_xywh_rectangle_filled(button_1[BTN_X],
-                               button_1[BTN_Y],
-                               button_1[BTN_WIDTH],
-                               button_1[BTN_HEIGHT],
-                               button_1[BTN_COLOR])
+                                      button_1[BTN_Y],
+                                      button_1[BTN_WIDTH],
+                                      button_1[BTN_HEIGHT],
+                                      button_1[BTN_COLOR])
 
 
 def draw_instruction_button(instruction_button):
